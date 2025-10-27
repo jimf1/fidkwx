110eb55f720ec6f610fc7fc0b6c9e4dd5738c8cf
Jim <jimf@fidk.com>
2025-10-27 00:22:19 -0400

    Initial Commit for dbvars
    To separate dbvars files to machine-specific values

---
+++ settings/dbvars-fidk.com.php
@@ -0,0 +1,9 @@
+<?php
+//** Define variable names for use in database functions **//
+    $hostname = "127.0.0.1:3308";
+    $user = "wxuser";
+    $password = "DaterTater123";
+    $schema = "fidk_wx";
+
+    
+?>
