910284687afaa542c49ecaac8461ff44f6152b18
Jim <jimf@fidk.com>
2025-10-27 10:17:58 -0400

    Fix dbvars.php overriting prod
    dbvars.php was overriding the production server with the local database details

--- settings/dbvars.php
+++
@@ -1,9 +0,0 @@
-<?php
-//** Define variable names for use in database functions **//
-    $hostname = "localhost";
-    $user = "wxuser";
-    $password = "weather";
-    $schema = "fidkwx";
-
-    
-?>
